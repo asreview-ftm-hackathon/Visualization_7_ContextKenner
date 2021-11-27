d821e9ec8451ca7c853421a74a0f574418747f76
Neele Dijkstra <neeledijkstra@hotmail.com>
2021-11-27 15:56:54 +0100

    restructure shiny app

--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -5,61 +5,50 @@ library(ggplot2)
 
 fluidPage(
   
-  # tags$head(
-  #   tags$link(rel = "stylesheet", type = "text/css", href = "stylesheet.css")
-  # ),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "stylesheet.css")
+  ),
   
   title="ContextKenner",
   
   fluidRow(
     id="header",
     
-    h1("ContextKenner"),
-    
-    p(
-      id="intro",
-      "Deze ContextKenner kan gebruikt worden om de woorden in de Shell files en hun context te doorzoeken. 
-      Type een woord in de Search bar om te beginnen."
+    column(12,
+      h1("ContextKenner"),
+           
+      p(id="intro",
+      "Deze ContextKenner kan gebruikt worden om de woorden in de Shell files en hun context te doorzoeken.
+      Onderstaande tabel rangschikt de Shell files op informatierijkheid.
+      Type een woord in de zoekbalk om de belangrijkste stukken met die term te vinden."
+      )
     )
     
   ),
   
   fluidRow(
-    id="raw",
+    id="data",
     
-    column(4,
-        checkboxGroupInput("show_vars", "Columns:",
-                              names(diamonds), selected = names(diamonds))
-    ),
-    
-    column(8,
+    column(12,
         DT::dataTableOutput("raw")
     )
     
   ),
   
   fluidRow(
-    id = "freq",
-    
-    column(6,
+    column(4,
+      id = "score",
+      
       h2("Belangrijk?"),
       verbatimTextOutput('bel')
     ),
     
-    column(6,
-      h2("Locatie"),
-      verbatimTextOutput('loc')
-    ),    
-    
-  ),
-  
-  fluidRow(
-    id = "context",
-    
-    h2("Context"),
-    verbatimTextOutput('ctxt'),
-    
-    plotOutput("context")
+    column(8,
+      id = "ctxt",
+      
+      p("Probeer ook eens deze verwante zoektermen:"),
+      plotOutput("context")
+    )
     
   )
  
